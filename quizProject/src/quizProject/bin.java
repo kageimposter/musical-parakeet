e56d99f2d06006daee30ba5fbbdfb1bc7097d1aa
kageimposter <kageimposter@gmail.com>
2016-05-19 10:19:29 -0500

    COMMIT 10---FIXED IT!!!

--- quizProject/src/quizProject/bin.java
+++ quizProject/src/quizProject/bin.java
@@ -1,10 +1,12 @@
 package quizProject;
 
-
+import java.awt.Window;
 
 public class bin{
 	public static void main(String[] args){
  quizFrame quizFrame = new quizFrame("Name");
- quizFrame.setVisible(true);
+ 
+// quizFrame2 quizFrame2 =  new quizFrame2("title");
+  //quizFrame2.setVisible(true);
 	}
 }
--- quizProject/src/quizProject/quizFrame.java
+++ quizProject/src/quizProject/quizFrame.java
@@ -3,14 +3,20 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.Graphics;
-import java.awt.GridLayout;
 import java.awt.Image;
 import java.awt.Toolkit;
+import java.awt.BorderLayout;
+import java.awt.Container;
+import javax.swing.Box;
+import javax.swing.ButtonGroup;
+import javax.swing.JCheckBox;
+import javax.swing.JFrame;
+import javax.swing.JRadioButton;
 
 import javax.swing.*;
 
 public class quizFrame extends JFrame{
-	//private Toolkit ig = Toolkit.getDefaultToolkit();
+	// Toolkit ig = Toolkit.getDefaultToolkit();
 	
 	
 	public quizFrame(String s){
@@ -19,51 +25,37 @@ public class quizFrame extends JFrame{
 		JFrame init = new JFrame("default");
 		init.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		init.setTitle(s);
+		init.setBounds(40,40,400,400);
 		//Image tittleIcon = ig.getImage(i);
-		//init.setIconImage(tittleIcon);
-		
-		
-		//fill the window with a useable panel space
-		
-	//	JPanel title = new insideFrame(getGraphics());
-		
-		JPanel insideframe = new insideFrame(getGraphics());
-		
-		//Layout for the Buttons
-		//BorderLayout titleQuestions = new BorderLayout(null);
-
-		Layout Answer = new BoxLayout(insideframe,BoxLayout.PAGE_AXIS );
-		super.setLayout(Answer);
-		
+		//init.setIconImage(tittleIcon)
 		
 		//Buttons with text
-		JButton b1 = new JButton("1");
-		 JButton b2 = new JButton("2");
-		 JButton b3 = new JButton("3");
-		 JButton b4 = new JButton("4");
-		 JButton b5 = new JButton("5");
-		 JButton b6 = new JButton("6");
-		    
-		   	super.add(b1);
-		    super.add(b2);
-		    super.add(b3);
-		    super.add(b4);
-		    super.add(b5);
-		    super.add(b6);
-		
-		  
-			
-		   
-		    
-		//set window size  and display it	
+	  	// add buttons to side
+	    Box text = Box.createVerticalBox();
+	    text.add(new JCheckBox("A"));
+	    text.add(new JCheckBox("B"));
+	    text.add(new JCheckBox("C"));
+	    text.add(new JCheckBox("d"));
+	    text.add(new JCheckBox("e"));
+	    text.add(new JButton("Next Question"));
+	    
+	    //the top container
+	    Box top = Box.createHorizontalBox();
+	    top.add(text);
+	 //   top.add(comp)
+	    Container content = init.getContentPane();
+	    content.setLayout(new BorderLayout());
+	    content.add(top, BorderLayout.CENTER);
+	    
+		//set window location  and display it	
 		    init.setLocationRelativeTo(null);
-		
+		init.setVisible(true);
 		
 	
 			}
+	public static void strings(){
 		
-
-
+	}
 }
 
 
