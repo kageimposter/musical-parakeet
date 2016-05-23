b52cef41b27a2ec8734de1c3d9bcdb82118d30e0
kage <kageimposter@gmail.com>
2016-05-23 05:20:04 +0000

    Home Commit 2 -- Added cust font and working on the q&a
    need to do the window listener still

--- quizProject/src/quizProject/quizFrame.java
+++ quizProject/src/quizProject/quizFrame.java
@@ -28,21 +28,50 @@ public class quizFrame extends JFrame{
 		init.setBounds(40,40,400,400);
 		//Image tittleIcon = ig.getImage();
 		//init.setIconImage(tittleIcon);
+		int section = 0;
 		
+		
+//get strings
+				String[] Questions = new String[10];
+				Questions[0] = "q";Questions[1] = "q";Questions[2] = "q";Questions[3] = "q";Questions[4] = "q";Questions[5] = "q";Questions[6] = "q";Questions[7] = "q";Questions[8] = "q";Questions[9] = "q";
+	
+				String[] Gryphondor = new String[10];
+				String[] RavenClaw = new String[10];
+				String[] Slyterin = new String[10];
+				String[] HonneyBadger = new String[10];
+				String[] Variates = new String[10];
+				
+				Gryphondor[0] = "A1";Gryphondor[1] = "A1";Gryphondor[2] = "A1";Gryphondor[3] = "A1";Gryphondor[4] = "A1";Gryphondor[5] = "A1";Gryphondor[6] = "A1";Gryphondor[7] = "A1";Gryphondor[8] = "A1";Gryphondor[9] = "A1";
+
+				RavenClaw[0] = "A2";RavenClaw[1] = "A2";RavenClaw[2] = "A2";RavenClaw[3] = "A2";RavenClaw[4] = "A2";RavenClaw[5] = "A2";RavenClaw[6] = "A2";RavenClaw[7] = "A2";RavenClaw[8] = "A2";RavenClaw[9] = "A2";
+
+				Slyterin[0] = "A3";Slyterin[1] = "A3";Slyterin[2] = "A3";Slyterin[3] = "A3";Slyterin[4] = "A3";Slyterin[5] = "A3";Slyterin[6] = "A3";Slyterin[7] = "A3";Slyterin[8] = "A3";Slyterin[9] = "A3";
+				
+				HonneyBadger[0] = "A4";HonneyBadger[1] = "A4";HonneyBadger[2] = "A4";HonneyBadger[3] = "A4";HonneyBadger[4] = "A4";HonneyBadger[5] = "A4";HonneyBadger[6] = "A4";HonneyBadger[7] = "A4";HonneyBadger[8] = "A4";HonneyBadger[9] = "A4";
+				
+				Variates[0] = "A5";Variates[1] = "A5";Variates[2] = "A5";Variates[3] = "A5";Variates[5] = "A5";Variates[6] = "A5";Variates[7] = "A5";Variates[8] = "A5";Variates[9] = "A5";
+				
+	
 	//Buttons with text
 	  	
 	    Box text = Box.createVerticalBox();
 	    //the text for the questions
-	    JTextArea question = new JTextArea(5,10);
+	    JTextArea question = new JTextArea(Questions[section]);
 	    question.setEditable(false);
+	   
+	    
+	    // question.setFont(f);
+	    question.setLineWrap(true);
+	    question.setWrapStyleWord(true);
 	    
 	    
+
 	    //buttons with options
-	    text.add(new JCheckBox("A"));
-	    text.add(new JCheckBox("B"));
-	    text.add(new JCheckBox("C"));
-	    text.add(new JCheckBox("d"));
-	    text.add(new JCheckBox("e"));
+	    text.add(new JCheckBox(Gryphondor[section]));
+	    text.add(new JCheckBox(RavenClaw[section]));
+	    text.add(new JCheckBox(Slyterin[section]));
+	    text.add(new JCheckBox(HonneyBadger[section]));
+	    text.add(new JCheckBox(Variates[section]));
 	  
 	    //next screen button
 	    text.add(new JButton("Next Question"));
@@ -65,24 +94,9 @@ public class quizFrame extends JFrame{
 		
 	
 			}
-	public static void strings(){
+	
 		
-		String[] Gryphondor = new String[10];
-		String[] RavenClaw = new String[10];
-		String[] Slyterin = new String[10];
-		String[] HonneyBadger = new String[10];
-		String[] Variates = new String[10];
 		
-		Gryphondor[0] = "A1";Gryphondor[1] = "A1";Gryphondor[2] = "A1";Gryphondor[3] = "A1";Gryphondor[4] = "A1";Gryphondor[5] = "A1";Gryphondor[6] = "A1";Gryphondor[7] = "A1";Gryphondor[8] = "A1";Gryphondor[9] = "A1";
-
-		RavenClaw[0] = "A2";RavenClaw[1] = "A2";RavenClaw[2] = "A2";RavenClaw[3] = "A2";RavenClaw[4] = "A2";RavenClaw[5] = "A2";RavenClaw[6] = "A2";RavenClaw[7] = "A2";RavenClaw[8] = "A2";RavenClaw[9] = "A2";
-
-		Slyterin[0] = "A3";Slyterin[1] = "A3";Slyterin[2] = "A3";Slyterin[3] = "A3";Slyterin[4] = "A3";Slyterin[5] = "A3";Slyterin[6] = "A3";Slyterin[7] = "A3";Slyterin[8] = "A3";Slyterin[9] = "A3";
-		
-		HonneyBadger[0] = "A4";HonneyBadger[1] = "A4";HonneyBadger[2] = "A4";HonneyBadger[3] = "A4";HonneyBadger[4] = "A4";HonneyBadger[5] = "A4";HonneyBadger[6] = "A4";HonneyBadger[7] = "A4";HonneyBadger[8] = "A4";HonneyBadger[9] = "A4";
-		
-		Variates[0] = "A5";Variates[1] = "A5";Variates[2] = "A5";Variates[3] = "A5";Variates[5] = "A5";Variates[6] = "A5";Variates[7] = "A5";Variates[8] = "A5";Variates[9] = "A5";
-		}
 }
 
 
