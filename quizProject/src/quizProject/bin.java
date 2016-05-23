7b93541648d9203b7fbbc9a0c98a9f5167045abc
kage <kageimposter@gmail.com>
2016-05-23 04:54:34 +0000

    commit from home 1-- strings in place need fill and var set up

--- quizProject/src/quizProject/bin.java
+++ quizProject/src/quizProject/bin.java
@@ -4,7 +4,7 @@ import java.awt.Window;
 
 public class bin{
 	public static void main(String[] args){
- quizFrame quizFrame = new quizFrame("Name");
+ quizFrame quizFrame = new quizFrame("Harry Potter Quiz");
  
 // quizFrame2 quizFrame2 =  new quizFrame2("title");
   //quizFrame2.setVisible(true);
--- quizProject/src/quizProject/quizFrame.java
+++ quizProject/src/quizProject/quizFrame.java
@@ -67,10 +67,22 @@ public class quizFrame extends JFrame{
 			}
 	public static void strings(){
 		
+		String[] Gryphondor = new String[10];
+		String[] RavenClaw = new String[10];
+		String[] Slyterin = new String[10];
+		String[] HonneyBadger = new String[10];
+		String[] Variates = new String[10];
 		
+		Gryphondor[0] = "A1";Gryphondor[1] = "A1";Gryphondor[2] = "A1";Gryphondor[3] = "A1";Gryphondor[4] = "A1";Gryphondor[5] = "A1";Gryphondor[6] = "A1";Gryphondor[7] = "A1";Gryphondor[8] = "A1";Gryphondor[9] = "A1";
+
+		RavenClaw[0] = "A2";RavenClaw[1] = "A2";RavenClaw[2] = "A2";RavenClaw[3] = "A2";RavenClaw[4] = "A2";RavenClaw[5] = "A2";RavenClaw[6] = "A2";RavenClaw[7] = "A2";RavenClaw[8] = "A2";RavenClaw[9] = "A2";
+
+		Slyterin[0] = "A3";Slyterin[1] = "A3";Slyterin[2] = "A3";Slyterin[3] = "A3";Slyterin[4] = "A3";Slyterin[5] = "A3";Slyterin[6] = "A3";Slyterin[7] = "A3";Slyterin[8] = "A3";Slyterin[9] = "A3";
 		
+		HonneyBadger[0] = "A4";HonneyBadger[1] = "A4";HonneyBadger[2] = "A4";HonneyBadger[3] = "A4";HonneyBadger[4] = "A4";HonneyBadger[5] = "A4";HonneyBadger[6] = "A4";HonneyBadger[7] = "A4";HonneyBadger[8] = "A4";HonneyBadger[9] = "A4";
 		
-	}
+		Variates[0] = "A5";Variates[1] = "A5";Variates[2] = "A5";Variates[3] = "A5";Variates[5] = "A5";Variates[6] = "A5";Variates[7] = "A5";Variates[8] = "A5";Variates[9] = "A5";
+		}
 }
 
 
